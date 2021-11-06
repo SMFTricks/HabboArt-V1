a59608e44bde17c6dede80fc719904208d0bb458
Diego Andrés <diegoandres_cortes@outlook.com>
2021-11-06 09:03:51 -0600

    gh release
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ languages/Settings.english.php
@@ -0,0 +1,9 @@
+<?php
+// Version: 2.0 RC5; Settings
+
+global $settings;
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'Habbo Art.<br /><br />Author: <a href="https://www.smftricks.com">SMF Tricks</a>';
+
+?>
